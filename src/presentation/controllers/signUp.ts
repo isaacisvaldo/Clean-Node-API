cb464ea3576e535f4d9e5302e52ca3f1c089b13d
Isaac Isvaldo <isaacisvaldobunga300@gmail.com>
2023-06-01 16:45:22 +0100

    refactor: simplificated requiredFileld

--- src/presentation/controllers/signUp.ts
+++ src/presentation/controllers/signUp.ts
@@ -3,11 +3,13 @@ import {MissingParamError} from '../errors/meassing-param-errors'
 import { badRequest } from '../helpers/http-helper'
 export class SignUpController {
     handle(httpRequest: httpRequest): httpResponse {
-        if(!httpRequest.body.name){
-            return badRequest(new MissingParamError('name'))
-        } else if (!httpRequest.body.email){
-            return badRequest(new MissingParamError('email'))
+        const requiredFields =['name','email']
+        for (const field of requiredFields){
+            if(!httpRequest.body[field]){
+                return badRequest(new MissingParamError(field))
+            }  
         }
+        
  
     }
 }
