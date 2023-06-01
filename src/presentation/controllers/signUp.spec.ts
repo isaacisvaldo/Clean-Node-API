c1f696b8765044609b8cda8bc1e87811c50af244
Isaac Isvaldo <isaacisvaldobunga300@gmail.com>
2023-06-01 15:45:36 +0100

    test: ensure return 400 if email is provided

--- src/presentation/controllers/signUp.spec.ts
+++ src/presentation/controllers/signUp.spec.ts
@@ -15,4 +15,18 @@ describe('signUp Controller', ()=> {
      expect(httpResponse.statusCode).toBe(400)
      expect(httpResponse.body).toEqual(new Error('Missing param: name'))
     })
+    test('Should return 400 if email is provided', ()=> {
+        const sut = new SignUpController()
+        const httpRequest = {
+           body: {
+               name:'Isaac Isvaldo',
+               password: 'password',
+               password_confirmation: 'password'
+   
+           }
+        }
+        const httpResponse = sut.handle(httpRequest)
+        expect(httpResponse.statusCode).toBe(400)
+        expect(httpResponse.body).toEqual(new Error('Missing param: email'))
+       })
 })
