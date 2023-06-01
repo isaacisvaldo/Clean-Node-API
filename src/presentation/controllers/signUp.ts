bd796d243eb41e260cb82cb3c325003dd3cddaea
Isaac Isvaldo <isaacisvaldobunga300@gmail.com>
2023-06-01 17:21:31 +0100

    feat: update controlers

--- src/presentation/controllers/signUp.ts
+++ src/presentation/controllers/signUp.ts
@@ -1,15 +1,17 @@
-import {httpRequest,httpResponse} from '../protocols/http'
+import {HttpRequest,HttpResponse} from '../protocols/http'
 import {MissingParamError} from '../errors/meassing-param-errors'
 import { badRequest } from '../helpers/http-helper'
-export class SignUpController {
-    handle(httpRequest: httpRequest): httpResponse {
-        const requiredFields =['name','email','password','password_confirmation']
-        for (const field of requiredFields){
-            if(!httpRequest.body[field]){
-                return badRequest(new MissingParamError(field))
-            }  
-        }
-        
+import {Controller} from '../protocols/controller'
+export class SignUpController  implements Controller {
+    handle(httpRequest: HttpRequest): HttpResponse {
+     const requiredFields =['name','email','password','password_confirmation']
+    for (const field of requiredFields){
+        if(!httpRequest.body[field]){
+            return badRequest(new MissingParamError(field))
+        }  
+    }  
  
-    }
+    }  
+    
+    
 }
