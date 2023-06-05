9873b402d91e1b4867ee259d66e894ce1413f7ba
Isaac Isvaldo <isaacisvaldobunga300@gmail.com>
2023-06-05 19:20:08 +0100

    feat: ensure signupcontroller retur 200 if valid data is provide

--- src/presentation/controllers/signup/signUp.ts
+++ src/presentation/controllers/signup/signUp.ts
@@ -30,11 +30,15 @@ export class SignUpController implements Controller {
             }
             //Se todos oa dados forem validados 
             //Criar um user 
-            this.addAccount.add({
+           const account= this.addAccount.add({
                 name,
                 email,
                 password
-            })
+              })
+              return {
+                statusCode: 200,
+                body: account
+              }
         } catch (error) {
             
             return serverError()
